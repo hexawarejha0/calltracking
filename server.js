adda63bc5a456d441a7bd40164a0c67206f3d947
rakeshjha143 <43853856+rakeshjha143@users.noreply.github.com>
2019-08-07 15:04:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 // Run server to listen on port 3000.
-const server = app.listen(3000, () => {
-  console.log('listening on *:3000');
+app.listen(PORT, () => {
+  console.log('listening on ${ PORT }');
 });
 
 app.use(bodyParser.urlencoded({ extended: false } ));
